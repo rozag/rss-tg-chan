1ceb6664db1f6018cb7fe50c17beef103890d6a0
Alexey Mileev <alemileev@gmail.com>
2018-10-29 19:39:46 +0300

    Remove retry.Policy struct

--- retry/retry.go
+++ retry/retry.go
@@ -4,14 +4,8 @@ import (
 	"time"
 )
 
-// Policy specifies retry policy (max number of retries, sleep duration and backoff multiplier)
-type Policy struct {
-	Retries int
-	Sleep   time.Duration
-	Backoff uint
-}
-
-func do(retries int, sleep time.Duration, backoff uint, job func() error) error {
+// Do tries to execute a job (func() error) with a number of retries, a sleep and a backoff multiplier
+func Do(retries uint, sleep time.Duration, backoff uint, job func() error) error {
 	if retries == 0 {
 		return job()
 	}
@@ -20,24 +14,5 @@ func do(retries int, sleep time.Duration, backoff uint, job func() error) error 
 		return nil
 	}
 	time.Sleep(sleep)
-	return do(retries-1, time.Duration(backoff)*sleep, backoff, job)
-}
-
-// Do tries to execute a job (func() error) with a number of retries, a sleep and a backoff multiplier
-func Do(policy *Policy, job func() error) error {
-	var sleep time.Duration
-	if policy.Sleep == 0 {
-		sleep = time.Second
-	} else {
-		sleep = policy.Sleep
-	}
-
-	var backoff uint
-	if policy.Backoff == 0 {
-		backoff = 2
-	} else {
-		backoff = policy.Backoff
-	}
-
-	return do(policy.Retries, sleep, backoff, job)
+	return Do(retries-1, time.Duration(backoff)*sleep, backoff, job)
 }
--- retry/retry_test.go
+++ retry/retry_test.go
@@ -8,19 +8,21 @@ import (
 
 func TestDo(t *testing.T) {
 	cases := []struct {
-		policy   *Policy
+		retries  uint
+		sleep    time.Duration
+		backoff  uint
 		exitOn   int
 		expected int
 	}{
-		{policy: &Policy{Retries: 0, Sleep: time.Microsecond, Backoff: 2}, exitOn: 1, expected: 1},
-		{policy: &Policy{Retries: 3, Sleep: time.Microsecond, Backoff: 2}, exitOn: 4, expected: 4},
-		{policy: &Policy{Retries: 0, Sleep: time.Microsecond, Backoff: 2}, exitOn: 2, expected: 1},
-		{policy: &Policy{Retries: 3, Sleep: time.Microsecond, Backoff: 2}, exitOn: 2, expected: 2},
+		{retries: 0, sleep: time.Microsecond, backoff: 2, exitOn: 1, expected: 1},
+		{retries: 3, sleep: time.Microsecond, backoff: 2, exitOn: 4, expected: 4},
+		{retries: 0, sleep: time.Microsecond, backoff: 2, exitOn: 2, expected: 1},
+		{retries: 3, sleep: time.Microsecond, backoff: 2, exitOn: 2, expected: 2},
 	}
 	for _, c := range cases {
 		c := c
 		counter := 0
-		Do(c.policy, func() error {
+		Do(c.retries, c.sleep, c.backoff, func() error {
 			if counter == c.exitOn {
 				return nil
 			}
@@ -28,7 +30,7 @@ func TestDo(t *testing.T) {
 			return errors.New("Failed")
 		})
 		if counter != c.expected {
-			t.Errorf("retries=%d backoff=%d exitOn=%d counter=%d expected=%d", c.policy.Retries, c.policy.Backoff, c.exitOn, counter, c.expected)
+			t.Errorf("retries=%d backoff=%d exitOn=%d counter=%d expected=%d", c.retries, c.backoff, c.exitOn, counter, c.expected)
 		}
 	}
 }
